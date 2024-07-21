3bdd4459f0ebcf6468755344d2680ac4deaeb7d5
ZhannaKhizrieva <zhanna-apteka@mail.ru>
2024-07-21 19:14:05 +0300

    add task1 list

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { formatDate } from './utils';
+import styles from './app.module.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export const App = () => {
+	const [value, setValue] = useState('');
+	const [list, setList] = useState([]);
+	const [error, setError] = useState('');
 
-export default App;
+	const validate = (val) => val.length >= 3;
+	const onInputButtonClick = () => {
+		const prompValue = prompt('Введите значение');
+		if (validate(prompValue)) {
+			setValue(prompValue);
+			setError('');
+		} else {
+			setError('Введенное значение должно содержать минимум 3 символа');
+		}
+	};
+
+	const onAddButtonClick = () => {
+		if (!validate(value)) return;
+		setList((prev) => [...prev, { id: Date.now(), value, date: new Date() }]);
+		setValue('');
+		setError('');
+	};
+
+	const isValueValid = validate(value);
+
+	return (
+		<div className={styles.app}>
+			<h1 className={styles['page-heading']}>Ввод значения</h1>
+			<p className={styles['no-margin-text']}>
+				Текущее значение <code>value</code>:"
+				<output className={styles['current-value']}>{value}</output>"
+			</p>
+			{error !== '' && <div className={styles.error}>{error}</div>}
+			<div className={styles['buttons-container']}>
+				<button className={styles.button} onClick={onInputButtonClick}>
+					Ввести новое
+				</button>
+				<button
+					className={styles.button}
+					onClick={onAddButtonClick}
+					disabled={!isValueValid}
+				>
+					Добавить в список
+				</button>
+			</div>
+			<div className={styles['list-container']}>
+				<h2 className={styles['list-heading']}>Список</h2>
+				{list.length > 0 ? (
+					<ul className={styles.list}>
+						{list.map((item) => (
+							<li className={styles['list-item']} key={item.id}>
+								{item.value} (
+								<time dateTime={item.date.toISOString()}>
+									{formatDate(item.date)}
+								</time>
+								L )
+							</li>
+						))}
+					</ul>
+				) : (
+					<p className={styles['no-margin-text']}>Нет добавленных элементов</p>
+				)}
+			</div>
+		</div>
+	);
+};
